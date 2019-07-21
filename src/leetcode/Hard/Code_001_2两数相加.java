ccaba4aa72875f6be8018ab853b9103d5bbfc566
ermaoyu <928432997@qq.com>
2019-07-21 21:34:25 +0800

    leetcode每日一刷--DAY2:7/21 2.两数相加(链表) 3.最长回文子串(滑动窗口)

---
+++ src/leetcode/Hard/Code_001_2两数相加.java
@@ -0,0 +1,43 @@
+package leetcode.Hard;
+
+import leetcode.ListNode;
+
+import java.util.List;
+
+/**
+ * maomaoyu    2019/7/21_20:58
+ * 给出两个空的链表用来表示两个非负的整数。其中，它们各自的位数是按照逆序的方式存储的，
+ * 并且它们的每个节点只能存储一位数字。
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * 示例：
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ **/
+public class Code_001_2两数相加 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode resNode = new ListNode(0);
+        ListNode q = l1,p = l2,cur = resNode;
+        int carry = 0;
+        while (p != null || q != null){
+            int x = (p != null ? p.val : 0);
+            int y = (q != null ? q.val : 0);
+            int sum = x + y + carry;
+            carry = sum /10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0){
+            cur.next = new ListNode(carry);
+        }
+        return resNode.next;
+    }
+
+}
---
+++ src/leetcode/Hard/Code_002_3无重复字符的最长子串.java
@@ -0,0 +1,44 @@
+package leetcode.Hard;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * maomaoyu    2019/7/21_21:21
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * 示例 1:
+ *
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ **/
+public class Code_002_3无重复字符的最长子串 {
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || "".equals(s))return 0;
+        int L = 0;
+        int R = 0;
+        int max = 0;
+        Map<Character,Integer> map = new HashMap<>();
+        for (; R < s.length(); R++) {
+            if (map.containsKey(s.charAt(R))){
+                L = Math.max(L, map.get(s.charAt(R)));
+            }
+            map.put(s.charAt(R), R+1);
+            max =Math.max(max,R - L + 1);
+        }
+        return max;
+    }
+}
